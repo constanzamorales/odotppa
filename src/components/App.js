1399b355dca3825c4c2ed488ee7043f5dbce6cb1
constanzamorales <constanzabmorales@gmail.com>
2022-09-24 11:32:38 -0300

    Created App and Field files

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Field from "./Field";
+
+class App extends React.Component {
+	render() {
+		return (
+			<div className="ui container">
+				<Field />
+			</div>
+		);
+	}
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./components/App";
 
 const element = document.getElementById("root");
 
 const root = ReactDOM.createRoot(element);
-
-function App() {
-	return <div>Hello world</div>;
-}
 
 root.render(<App />);
