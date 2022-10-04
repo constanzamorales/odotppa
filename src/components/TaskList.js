98eb280a18d32be74e02e2c1708b74c971694cb6
constanzamorales <constanzabmorales@gmail.com>
2022-10-04 18:33:40 -0300

    Added props and checkbox to setList

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,19 @@
 import React from "react";
 import Checkbox from "./Checkbox";
 
-const TaskList = () => {
+const TaskList = (props) => {
+	const { list, setList } = props;
+
+	const check = list.map((item) => <Checkbox key={item.id} data={item} />);
+
 	return (
-		<div>
+		<div className="todo-list">
+			{list.length ? check : "There are no tasks yet"}
+			{list.length ? (
+				<p>
+					<button className="ui button">Delete completed tasks</button>
+				</p>
+			) : null}
 			<div>TaskList works</div>
 			<Checkbox />
 		</div>
