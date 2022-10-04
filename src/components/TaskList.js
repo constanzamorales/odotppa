cc3eb5cda3dd2c33f2c6533096a50d9c62e8c29a
constanzamorales <constanzabmorales@gmail.com>
2022-10-04 18:38:50 -0300

    Added onChange function so now everytime the user submits a task, the component Checkbox appears

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,7 +4,18 @@ import Checkbox from "./Checkbox";
 const TaskList = (props) => {
 	const { list, setList } = props;
 
-	const check = list.map((item) => <Checkbox key={item.id} data={item} />);
+	const onChange = (e) => {
+		const { name, checked } = e.target;
+		const updateList = list.map((item) => ({
+			...item,
+			done: item.id === name ? checked : item.done
+		}));
+		setList(updateList);
+	};
+
+	const check = list.map((item) => (
+		<Checkbox onChange={onChange} key={item.id} data={item} />
+	));
 
 	return (
 		<div className="todo-list">
