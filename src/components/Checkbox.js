a8dfc0a6acda8e2c6385991036a6142f3705ff30
constanzamorales <constanzabmorales@gmail.com>
2022-10-05 19:54:00 -0300

    Added functionality to the Checkbox component. Now we can delete tasks successfully.

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-const Checkbox = () => {
-	return <div>Checkbox works</div>;
+const Checkbox = (props) => {
+	const {
+		onChange,
+		data: { id, task, done }
+	} = props;
+
+	return (
+		<>
+			<div className="ui checkbox">
+				<input
+					className=" todo__state"
+					name={id}
+					type="checkbox"
+					defaultChecked={done}
+					onChange={onChange}
+				/>
+				<label className=" todo new-item">{task}</label>
+			</div>
+		</>
+	);
 };
 
 export default Checkbox;
